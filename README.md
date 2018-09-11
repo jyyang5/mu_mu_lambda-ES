b5bfe200bc4b6531925dceeadc82c5ad88f51820
Jingyun Yang <jingyunyang@T8FCA.WPA.Dal.Ca>
2018-09-11 08:34:17 -0300

    Simple mml-ES
    mml-ES

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mu_mu_lambda-ES
+Implement a $(\mu /\mu,\lambda )-ES$ 
+It is initialized with $\mu$ parents, choose the centroid of parents to generate $\lambda$ offsprings and then pick the $\mu$ best offspring as the parents of next iteration. 
+**Note**: $\lambda \geq \mu$ 
---
+++ run_mml.m
@@ -0,0 +1,27 @@
+%function val = mml(f,x0,sigma_star,sigma_ep_star,lambda,sigma0,NUM_OF_ITERATIONS)
+% initialization
+% f:                  objective function value
+% x0:                 mu initial point
+% mu:                 population size
+% sigma0:             initial muttaion strength
+% NUM_OF_ITERATIONS:  number of maximum iterations
+
+
+
+% OPTIMAL:            global optima
+% TARGET_DISTANCE:    target distance to global optima
+% example input:      fun = @(x) x' * x
+%                     noGP(fun, randn(10,1),1,1000) 
+f = @(x) (x'*x);
+n = 2;
+mu = 3;
+lambda = 6;
+x0 = randn(n,mu)*100;
+sigma_star = 1;
+sigma_ep_star = 0;
+sigma0 = 1;
+NUM_OF_ITERATIONS = 5000;
+a = mml(f,x0,sigma_star,sigma_ep_star,lambda,sigma0,NUM_OF_ITERATIONS);
+centroid = cell2mat(a(2));
+fcentroid = cell2mat(a(3));
+sigma_array = cell2mat(a(4));
