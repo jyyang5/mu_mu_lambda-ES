4f67d43a7055e0066dc48191d515ee8afc3f10a9
Jingyun Yang <jingyunyang@T8FCA.WPA.Dal.Ca>
2018-09-11 16:26:23 -0300

    mml-es with different noise level
    complete the plot for different noise-to-signal ratio

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Implement a $(\mu /\mu,\lambda )-ES$ 
 It is initialized with $\mu$ parents, choose the centroid of parents to generate $\lambda$ offsprings and then pick the $\mu$ best offspring as the parents of next iteration. 
 **Note**: $\lambda \geq \mu$ 
+
+## Different noise level
+Plot the performance of mml-ES under different noise-to-signal ratio defined as $v = \frac{\sigma_\epsilon^star}{\sigma^star} $ 
+The undesired pattern for green dots is that the convergence rate is nan.
+Under high $v$ and large $\sigma^*$ the algorithm does not converge, which gives the negative convergence rate.
---
+++ run_mml_noise.m
@@ -0,0 +1,34 @@
+%function val = mml(f,x0,sigma_star,sigma_ep_star,lambda,sigma0,NUM_OF_ITERATIONS)
+% initialization
+% f:                  objective function value
+% x0:                 mu initial point
+% mu:                 population size
+% sigma0:             initial muttaion strength
+% NUM_OF_ITERATIONS:  number of maximum iterations
+
+
+
+% OPTIMAL:            global optima
+% TARGET_DISTANCE:    target distance to global optima
+% example input:      fun = @(x) x' * x
+%                     noGP(fun, randn(10,1),1,1000) 
+f = @(x) (x'*x);
+n = 10;
+mu = 2;
+lambda = 5;
+x0 = randn(n,mu);
+sigma_star = 1/2;
+sigma_ep_star = 4*sigma_star;
+NUM_OF_ITERATIONS = 5000;
+a = mml_noise(f,x0,sigma_star,sigma_ep_star,lambda,NUM_OF_ITERATIONS);
+t = cell2mat(a(1));
+disp("number of iterations");
+disp(t);
+
+centroid = cell2mat(a(5));
+fcentroid = cell2mat(a(6));
+sigma_array = cell2mat(a(4));
+convergence_rate = cell2mat(a(7));
+
+disp("convergence rate");
+disp(convergence_rate);
